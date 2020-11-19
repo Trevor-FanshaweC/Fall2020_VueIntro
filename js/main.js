afcd63e17bf4c67a52e4d4c7caaccbd21a7af509
tvanrys <tvanrys@fanshaweonline.ca>
2020-11-19 15:01:53 -0500

    latest Vue stuff

--- js/main.js
+++ js/main.js
@@ -6,12 +6,29 @@
         data: {
             message: "Hello from Vue!",
             anotherMessage: "more text, so simple! much winning",
+            removeAformat: true,
+            showBioData: false,
+            currentProfData: {},
 
-            profs: [
+            professors: [
                 { name: "Justin", role: "coordinator", nickname: "nitsuJ"},
                 { name: "John", role: "prof", nickname: "super chill" },
-                { name: "Joe", role: "prof", nickname: "Teddy Bear" }
+                { name: "Joe", role: "prof", nickname: "Teddy Bear" },
             ]
+        },
+
+        // this is the "mounted" lifecycle hook. Vue is done creating itself, and has attached itself to the "app" div on the page
+        mounted: function() {
+            console.log("Vue is mounted!");
+            // alert("Hey there! your vue instance is ready");
+
+            this.professors.push({name: "Jarrod", role: "supermodel prof", nickname: "Zoolander"} )
+            // alert("you added Jarrod!")
+        },
+
+        // run a method when we change our view (update the DOM with Vue)
+        updated: function() {
+            console.log('Vue just updated the DOM');
         },
 
         methods: {
@@ -21,6 +38,16 @@
 
             clickHeader() {
                 console.log("clicked on the header");
+            },
+
+            removeProf(target) {
+                // remove this prof from the professors array
+                console.log('clicked to remove prof', target, target.name);
+                // the "this" keyword inside a vue instance REFERS to the Vue instance itself by default
+
+                // toggle the property between true and false using a ternary statement
+                this.showBioData = this.showBioData ? false : true
+                this.currentProfData = target;
             }
         }
     }).$mount("#app"); // also connects Vue to your wrapper in HTML
